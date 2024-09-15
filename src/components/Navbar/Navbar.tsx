91398cb915eaf9c427478ad139c5cb405d4f628f
YahyaKng <yhy.kng@gmail.com>
2024-09-15 15:27:40 +0330

    removed react-window

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import { useRouter } from "next/navigation";
 
 export default function Navbar() {
